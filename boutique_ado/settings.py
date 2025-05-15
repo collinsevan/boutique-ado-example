eae06af2fac28ee385448969462b6df922a43038
collinsevan <collinsevan33@gmail.com>
2025-05-15 15:07:45 +0100

    added checkout views and templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -47,6 +47,9 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+
+    # Other
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -59,7 +62,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'boutique_ado.urls'
+ROOT_URLCONF = 'boutique_ado.urls'#
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -75,8 +80,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',  # noqa, required by allauth do not remove
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_feilds',
+            ]
         },
     },
 ]
